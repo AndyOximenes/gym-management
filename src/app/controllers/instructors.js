ae092939675c68e916f859b17a634604347018e4
AndyOximenes <andy.oximenes@gmail.com>
2020-07-17 14:50:45 -0300

    insert data into database

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,4 +1,5 @@
 const { age, date } = require("../../lib/utils");
+const db = require("../../app/config/db");
 
 module.exports = {
   index(request, response) {
@@ -11,7 +12,6 @@ module.exports = {
 
   post(request, response) {
     const keys = Object.keys(request.body);
-    let { avatar_url, name, birth, gender, services } = request.body;
 
     for (key of keys) {
       if (request.body[key] == "") {
@@ -19,7 +19,32 @@ module.exports = {
       }
     }
 
-    return;
+    const query = `
+      INSERT INTO instructors (
+        name,
+        avatar_url,
+        gender,
+        services,
+        birth,
+        created_at
+      ) VALUES ($1, $2, $3, $4, $5, $6)
+      RETURNING id
+    `;
+
+    const values = [
+      request.body.name,
+      request.body.avatar_url,
+      request.body.gender,
+      request.body.services,
+      date(request.body.birth).iso,
+      date(Date.now()).iso,
+    ];
+
+    db.query(query, values, (err, results) => {
+      if (err) return response.send("Database error");
+
+      return response.redirect(`/instructors/${results.rows[0].id}`);
+    });
   },
 
   create(request, response) {
