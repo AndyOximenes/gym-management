af4a3dc563ef45026932bf2669d6a22cc10494da
AndyOximenes <andy.oximenes@gmail.com>
2020-07-28 16:14:02 -0300

    Adjusting scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -16,3 +16,19 @@ formDelete.addEventListener("submit", (e) => {
     e.preventDefault();
   }
 });
+
+// PAGINAÇÃO
+
+let totalPages = 20,
+  selectedPage = 12;
+pages = [];
+
+for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+  const firstAndLastPage = currentPage == 1 || currentPage == totalPages;
+  const pagesAfterSelectedPage = currentPage <= selectedPage + 2;
+  const pagesBeforeSelectedPage = currentPage >= selectedPage - 2;
+
+  if (firstAndLastPage || (pagesBeforeSelectedPage && pagesAfterSelectedPage)) {
+    pages.push(currentPage);
+  }
+}
